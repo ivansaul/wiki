7a6c962dc08e500129821c5f6f0c3a40568c7f67
ivansaul <saul_2503@hotmail.com>
2024-02-03 22:37:15 -0500

    chore: update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ivansaul.github.io/wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
